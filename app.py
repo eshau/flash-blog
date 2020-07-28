1419505b9a8f39be5764f05b7632e1b1d89d9ed0
EShau00 <EShau00@gmail.com>
2020-07-28 15:01:57 +0000

    Added +New Post

--- app.py
+++ app.py
@@ -3,14 +3,22 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import url_for
+from flask_pymongo import PyMongo
+import os
+from dotenv import load_dotenv
 from datetime import datetime
+import model
 
 # -- Initialization section --
 app = Flask(__name__)
 
-
 # -- Routes section --
 @app.route('/')
-@app.route('/index')
+@app.route('/index/')
 def index():
     return render_template("index.html", time=datetime.now())
+
+@app.route('/new_post/')
+def new_post():
+    return render_template("new_post.html", time=datetime.now())
