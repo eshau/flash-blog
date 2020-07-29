b4222a83c678bc76522bc8e1697de209efe47f7c
Axgel <liangel421@gmail.com>
2020-07-29 11:07:00 -0400

    added button functionality

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask import request
 from flask import url_for
 from flask import redirect
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 import os
 from dotenv import load_dotenv
 from datetime import datetime
@@ -36,28 +37,51 @@ def new_post():
     if request.method == 'GET':
         return render_template('new_post.html', time=datetime.now())
     else:
-        books = mongo.db['books-list']
+        book_reviews = mongo.db['books-list']
         form = request.form
         book_title = form["book_title"]
         post_title = form["post_title"]
         author = form["author"]
         genres = form.getlist('genres')
+        image_link = form['image_link']
         review = form["review"]
         rating = form["rating"]
+        now = datetime.now()
+        date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
         book_review = {
             "book_title" : book_title,
             "post_title" : post_title,
             "author" : author,
             "genres" : genres,
+            "image_link" : image_link,
             "review" : review,
             "rating" : rating,
+            "time" : date_time,
         }
-        books.insert(book_review)
-        data = {
-        'book_reviews':mongo.db["books-list"].find({}),
-        }
-        return render_template('home_page.html', data=data, time=datetime.now())
+        book_reviews.insert(book_review)
+        return redirect(url_for('home_page'))
 
-@app.route('/blog_post')
-def blog_post():
-    return render_template('blog_post.html')
+@app.route('/blog_post/<book_review_id>')
+def blog_post(book_review_id):
+    book_reviews = mongo.db["books-list"]
+    book_review_id = ObjectId(book_review_id)
+    book_review = book_reviews.find_one({'_id':book_review_id})
+    return render_template('blog_post.html', book_review=book_review, time=datetime.now())
+
+@app.route('/sort_by/<book_review_tag>')
+def sort_by(book_review_tag):
+    book_reviews = mongo.db["books-list"]
+    genres =   ["Action/Adventure", "Classics", "Comic/Graphic Novel", "Detective/Mystery",
+                "Fantasy", "Historical Fiction", "Horror", "Literary Fiction",
+                "Romance", "Sci-Fiction", "Short-Stories", "Suspense/Thrillers",
+                "Biographies", "Memoirs", "Poetry", "Other",
+                ]
+    if book_review_tag in genres:
+        data = {
+            'book_reviews':mongo.db["books-list"].find({'genres':book_review_tag})
+        }
+    else:
+        data = {
+            'book_reviews':mongo.db["books-list"].find({'author':book_review_tag})
+        }
+    return render_template('home_page.html', data=data, time=datetime.now())    
